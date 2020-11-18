b5caa6f0cf46d7326beef887a80cc2a2daee073e
danabliss <danajackson2@gmail.com>
2020-11-18 10:13:33 -0800

    dana commit

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,23 @@
-# your code goes here
+class Person
+
+    attr_reader :name, :happiness
+    attr_accessor :bank_account
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+    end
+
+    def happiness=(num)
+        if num <= 10 && num >= 0
+            @happiness = num
+        elsif num > 10
+            @happiness = 10
+        elsif num < 0
+            @happiness = 0
+        end
+    end
+
+
+end
