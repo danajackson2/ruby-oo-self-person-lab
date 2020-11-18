e78f603ef86358a18ef6e5bb64c15f39610ce2f4
Zoe Kirsh <zoekirsh@gmail.com>
2020-11-18 10:22:30 -0800

    accessors done

--- lib/person.rb
+++ lib/person.rb
@@ -1,12 +1,13 @@
 class Person
 
-    attr_reader :name, :happiness
+    attr_reader :name, :happiness, :hygiene
     attr_accessor :bank_account
 
     def initialize(name)
         @name = name
         @bank_account = 25
         @happiness = 8
+        @hygiene = 8
     end
 
     def happiness=(num)
@@ -18,6 +19,17 @@ class Person
             @happiness = 0
         end
     end
+
+    def hygiene=(num)
+        if num <= 10 && num >= 0
+            @hygiene = num
+        elsif num > 10
+            @hygiene = 10
+        elsif num < 0
+            @hygiene = 0
+        end
+    end
+
 
 
 end
