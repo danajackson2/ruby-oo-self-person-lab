6017683e74aef14280b2c585b8f9944369231170
Lucas Montanari <Lmonty2011@gmail.com>
2020-11-18 11:09:13 -0800

    lucas commits

--- lib/person.rb
+++ lib/person.rb
@@ -46,12 +46,34 @@ class Person
     end
 
     def take_bath
-        clean = @hygiene + 4
-        self.hygiene=(clean)
+        self.hygiene=(@hygiene + 4)
         "♪ Rub-a-dub just relaxing in the tub ♫"
     end
 
+    def work_out
+        self.happiness = (@happiness + 2)
+        self.hygiene = (@hygiene - 3)
+        "♪ another one bites the dust ♫"
+    end
 
+    def call_friend(friend)
+        self.happiness = (@happiness + 3)
+        friend.happiness = (friend.happiness + 3)
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
 
-
+    def start_conversation(friend, topic)
+        case topic
+        when "politics"
+            self.happiness = (self.happiness - 2)
+            friend.happiness = (friend.happiness - 2)
+            "blah blah partisan blah lobbyist"
+        when "weather"
+            self.happiness = (self.happiness + 1)
+            friend.happiness = (friend.happiness + 1)
+            "blah blah sun blah rain"
+        else
+            "blah blah blah blah blah"
+        end
+    end
 end
